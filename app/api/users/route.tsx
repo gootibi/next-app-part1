6d4f5fe54c53f6c1e154c7f59f661af3c3e9c97e
gootibi <gootibi@gmail.com>
2024-03-20 00:49:28 +0100

    Creating an Object - POST - POSTMAN to check the POST request.

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -4,6 +4,7 @@
 
 import { NextRequest, NextResponse } from "next/server";
 
+// POSTMAN: http://localhost:3000/api/users - GET
 export function GET(request: NextRequest) {
     // Fetch users from a database.
     // Now not fetching, this is hardcoded example.
@@ -11,4 +12,15 @@ export function GET(request: NextRequest) {
         { id: 1, name: 'Tibi' },
         { id: 2, name: 'John' },
     ])
+}
+
+// POSTMAN: http://localhost:3000/api/users - POST - body - json - create data
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+
+    if (!body.name) { // Bad request, name is required (empty)
+        return NextResponse.json({ error: 'Name is required'}, { status: 400})
+    }
+
+    return NextResponse.json({ id: 1, name: body.name }, { status: 201 }) // Success, status code is 201 => created
 }
