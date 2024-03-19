16e27ba69b06ea6b6104f3d60279a575aa1a85dd
gootibi <gootibi@gmail.com>
2024-03-19 23:07:16 +0100

    Show Loading UIs - <Suspense> react element or loading.tsx and Daisy UI

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    // <div>Loading...</div>
+    <span className="loading loading-spinner loading-md"></span>
+  )
+}
+
+export default Loading
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,5 @@
 // http://localhost:3000/users/[id]/photos/[photoId]
-import React from 'react'
+import React, { Suspense } from 'react'
 import UserTable from './UserTable'
 import Link from 'next/link'
 
@@ -8,12 +8,14 @@ interface Props {
 }
 
 const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
-  
+
   return (
     <>
       <h1>Users</h1>
       <Link href='/users/new' className='btn'>New User</Link>
-      <UserTable sortOrder={sortOrder}/>
+      <Suspense fallback={<p>Loading...</p>} >
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </>
   )
 }
