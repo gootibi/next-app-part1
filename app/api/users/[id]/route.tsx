4212f99eb937cd6b9ed092e05660c021ae3c5007
gootibi <gootibi@gmail.com>
2024-03-20 01:12:44 +0100

    Updating an Object - PUT and PATCH - POSTMAN visit

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -14,3 +14,25 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
 
     return NextResponse.json({ id: 1, name: 'Tibi' })
 }
+
+// Visit result http://localhost:3000/api/users/[id] => [id] = number 1 or 13 ect. POSTMAN - PUT - body json
+// PATCH => update the single data element --- PUT => update the all data element
+export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
+    // Validate the request body
+    // If invalid return 400
+    const body = await request.json()
+    if (!body.name) { // body.name is required
+        return NextResponse.json({ error: 'Name is required' }, { status: 400 })
+    }
+
+    // Fetch the user with the given id
+    // If doesn't exist, return 404
+    if (params.id > 10) { // id greater than 10
+        return NextResponse.json({ error: `User not found, invalid id: ${params.id}` }, { status: 404 })
+    }
+
+    // Update the user
+    // Return the updated user
+    return NextResponse.json({ id: params.id, name: body.name }, { status: 200 })
+
+}
