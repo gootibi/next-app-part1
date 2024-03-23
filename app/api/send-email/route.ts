124572d788fe17f410eee13b1c57bc7c2288ffdc
gootibi <gootibi@gmail.com>
2024-03-23 01:13:43 +0100

    Send email bug fix 1

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -4,13 +4,13 @@ import { NextResponse } from 'next/server'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST() {
-    await resend.emails.send({
-        from: '...', // Add domain in https://resend.com/domains
-        to: 'gootibi@gmail.com',
-        subject: '...',
-        react: WelcomeTemplate({name: 'gootibi'})
-    })
+// export async function POST() {
+//     await resend.emails.send({
+//         from: '...', // Add domain in https://resend.com/domains
+//         to: 'gootibi@gmail.com',
+//         subject: '...',
+//         react: WelcomeTemplate({name: 'gootibi'})
+//     })
 
-    return NextResponse.json({})
-}
+//     return NextResponse.json({})
+// }
